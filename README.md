9a62622d68308ec2c0c3ec5399a5e290d98a1eff
Al-Alloush <ahmad.alloush@gmail.com>
2021-06-09 14:26:32 +0200

    add header & nav-bar

--- README.md
+++ README.md
@@ -6,9 +6,11 @@ npm i -g typescript
 npm i ngx-bootstrap 
 npm install bootstrap
 npm install font-awesome 
+npm install @angular/flex-layout
 
 ```
 ## install  [Angular Material](https://material.angular.io/guide/getting-started):
 ```
 ng add @angular/material
 ```
+### add nav-bar from [here](https://stackblitz.com/edit/responsive-menu-angular-material-flex-layout?file=src%2Fapp%2Fapp.module.ts)
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,20 +1,39 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HeaderComponent } from './header/header.component';
+
+// Angular Material
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+
+//
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 
 
 @NgModule({
   declarations: [
-    NavBarComponent
+    HeaderComponent
   ],
   imports: [
     CommonModule,
-    MatToolbarModule
+    //
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    //
+    FlexLayoutModule
+
   ],
   exports: [
-    NavBarComponent
+    HeaderComponent
   ]
 })
 export class SharedModule { }
